082edb8ee9506e31f0285b8303f0020a5f2583fc
JeanCar0922 <113635640+JeanCar0922@users.noreply.github.com>
2022-09-15 19:17:26 -0500

    afregamos un animal mas

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -3,7 +3,9 @@ public class GeneradorVertebrados {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
+        miVertebrados[2] = new Vertebrado("Tiburón", "Pez");
+        miVertebrados[3] = new Vertebrado("caballo", "Mamifero");
+        miVertebrados[4] = new Vertebrado("anguila", "Pez");  
         
         return miVertebrados;
     }
